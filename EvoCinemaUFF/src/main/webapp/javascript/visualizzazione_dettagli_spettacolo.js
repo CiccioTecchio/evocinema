76fcd12139304257f5f68edbb288f88feaa3272c
Luca <strluca94@gmail.com>
2018-01-25 12:09:24 +0100

    toggle

--- EvoCinemaUFF/src/main/webapp/javascript/visualizzazione_dettagli_spettacolo.js
+++ EvoCinemaUFF/src/main/webapp/javascript/visualizzazione_dettagli_spettacolo.js
@@ -13,6 +13,10 @@ function handleMouseOver(e){
 
 function handleClick(e) {
     var el = e.target;
-    el.setAttribute("src", "images/poltrona_selezionata_v2.png");
+    if(el.getAttribute("src").localCompare("images/poltrona_disponibile_v2.png") == 0){
+        el.setAttribute("src", "images/poltrona_selezionata_v2.png");    
+    } else if(el.getAttribute("src").localCompare("images/poltrona_selezionata_v2.png") == 0){
+        el.setAttribute("src", "images/poltrona_disponibile_v2.png");
+    }
 }
 
