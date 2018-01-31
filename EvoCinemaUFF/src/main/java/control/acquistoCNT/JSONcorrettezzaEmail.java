360940be55d9022dcab8fe8e26cc6a1cc62c4920
Pietro Dell'isola <Dellisola.pietro@gmail.com>
2018-01-31 11:33:06 +0100

    Modifiche a gestione acquisto

---
+++ EvoCinemaUFF/src/main/java/control/acquistoCNT/JSONcorrettezzaEmail.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.acquistoCNT;
+
+import database.UtenteRegistratoDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.UtenteBase;
+import model.UtenteRegistrato;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author pietr
+ */
+@WebServlet(name = "JSONcorrettezzaEmail", urlPatterns = {"/JSONcorrettezzaEmail"})
+public class JSONcorrettezzaEmail extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NamingException, SQLException, ParseException {
+        
+        
+        
+        response.setContentType("application/json;charset=UTF-8");
+        //PrintWriter out = response.getWriter();
+        
+        String emailAcquirente=request.getParameter("emailAcquirente");
+        UtenteRegistratoDAO urd = new UtenteRegistratoDAO();
+        JSONObject jsonObject=new JSONObject();
+        System.out.println("emailAcquirente "+emailAcquirente);
+        UtenteBase acquirente = urd.foundUtenteBaseByEmail(emailAcquirente);
+        
+        
+        if((null!=acquirente)&&(acquirente.getRuolo()==UtenteRegistrato.ruolo.UTENTE))
+            jsonObject.put("Email corretta", 0); 
+        else jsonObject.put("Email non corretta", 0);
+        
+        
+        System.out.println("messaggio json"+jsonObject.toString());
+        
+        response.getWriter().write(jsonObject.toString());
+
+
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(JSONcorrettezzaEmail.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(JSONcorrettezzaEmail.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(JSONcorrettezzaEmail.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(JSONcorrettezzaEmail.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(JSONcorrettezzaEmail.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(JSONcorrettezzaEmail.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
